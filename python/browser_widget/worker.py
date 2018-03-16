e822cd1593849577b2c267cc95c649535ea6ca09
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2018-03-16 13:43:27 -0400

    fixed qwaitcondition

--- python/browser_widget/worker.py
+++ python/browser_widget/worker.py
@@ -1,35 +1,34 @@
 # Copyright (c) 2013 Shotgun Software Inc.
-# 
+#
 # CONFIDENTIAL AND PROPRIETARY
-# 
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
 # Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your 
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-import os
-import urllib
 import uuid
-import sys
 
-from tank.platform.qt import QtCore, QtGui
+from tank.platform.qt import QtCore
+from threading import Lock, Condition
+
 
 class Worker(QtCore.QThread):
     """
     Background worker class
     """
-    
-    # Indicates that this worker class has been fixed to stop 
-    # gc of QThread from resulting in a crash.  This happens 
-    # when the mutex object had been gc'd but the thread is 
-    # still trying to acces it - the fix is to wait for the 
+
+    # Indicates that this worker class has been fixed to stop
+    # gc of QThread from resulting in a crash.  This happens
+    # when the mutex object had been gc'd but the thread is
+    # still trying to access it - the fix is to wait for the
     # thread to terminate before returning from 'stop()'
-    _SGTK_IMPLEMENTS_QTHREAD_CRASH_FIX_=True    
-    
+    _SGTK_IMPLEMENTS_QTHREAD_CRASH_FIX_ = True
+
     work_completed = QtCore.Signal(str, object)
     work_failure = QtCore.Signal(str, str)
-    
+
     def __init__(self, app, parent=None):
         """
         Construction
@@ -37,19 +36,22 @@ class Worker(QtCore.QThread):
         QtCore.QThread.__init__(self, parent)
         self._execute_tasks = True
         self._app = app
-        
-        self._queue_mutex = QtCore.QMutex()
+
+        self._queue_mutex = Lock()
+
         self._queue = []
         self._receivers = {}
-        
-        self._wait_condition = QtCore.QWaitCondition()
-        
+
+        self._wait_condition = Condition(self._queue_mutex)
+
     def stop(self, wait_for_completion=True):
         """
         Stops the worker, run this before shutdown
         """
-        self._execute_tasks = False
-        self._wait_condition.wakeAll()
+        with self._queue_mutex:
+            self._execute_tasks = False
+            self._wait_condition.notifyAll()
+
         if wait_for_completion:
             self.wait()
 
@@ -57,32 +59,26 @@ class Worker(QtCore.QThread):
         """
         Empties the queue
         """
-        self._queue_mutex.lock()
-        try:
+        with self._queue_mutex:
             self._queue = []
-        finally:
-            self._queue_mutex.unlock()
-        
+
     def queue_work(self, worker_fn, params, asap=False):
         """
         Queues up some work.
-        Returns a unique identifier to identify this item 
+        Returns a unique identifier to identify this item
         """
         uid = uuid.uuid4().hex
-        
+
         work = {"id": uid, "fn": worker_fn, "params": params}
-        self._queue_mutex.lock()
-        try:
+        with self._queue_mutex:
             if asap:
                 # first in the queue
                 self._queue.insert(0, work)
             else:
                 self._queue.append(work)
-        finally:
-            self._queue_mutex.unlock()
 
-        self._wait_condition.wakeAll()
-        
+            self._wait_condition.notifyAll()
+
         return uid
 
     ############################################################################################
@@ -91,24 +87,21 @@ class Worker(QtCore.QThread):
     def run(self):
 
         while self._execute_tasks:
-            
+
             # get the next item to process:
             item_to_process = None
-            self._queue_mutex.lock()
-            try:
+            with self._queue_mutex:
                 if len(self._queue) == 0:
                     # wait for some more work - this unlocks the mutex
                     # until the wait condition is signalled where it
                     # will then attempt to obtain a lock before returning
-                    self._wait_condition.wait(self._queue_mutex)
-                    
+                    self._wait_condition.wait()
+
                     if len(self._queue) == 0:
                         # still nothing in the queue!
                         continue
-                
+
                 item_to_process = self._queue.pop(0)
-            finally:
-                self._queue_mutex.unlock()
 
             if not self._execute_tasks:
                 break
@@ -124,4 +117,3 @@ class Worker(QtCore.QThread):
             else:
                 if self._execute_tasks:
                     self.work_completed.emit(item_to_process["id"], data)
-                
